00f5f8bdac355eca52a915b516cf860eef799533
Dashkka19 <167982504+Dashkka19@users.noreply.github.com>
2024-04-24 14:52:52 +0300

    Add files via upload
    Программа магазина цветов

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Cveti.Menu;
+using Cveti.Menu.ForClients;
+using System.Windows;
+
+namespace Cveti
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Administrator_Click(object sender, RoutedEventArgs e)
+        {
+            Proverka proverka = new Proverka();
+            proverka.Show();
+        }
+
+        private void Meneger_Click(object sender, RoutedEventArgs e)
+        {
+            Proverka proverka = new Proverka();
+            proverka.Show();
+        }
+
+        private void Kassir_Click(object sender, RoutedEventArgs e)
+        {
+            Proverka proverka = new Proverka();
+            proverka.Show();
+        }
+
+        private void Client_Click(object sender, RoutedEventArgs e)
+        {
+            Proverca proverca = new Proverca();
+            proverca.Show();
+        }
+    }
+}
